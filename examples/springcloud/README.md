63b9a577d2bc1cdef478e7cf65a4808721997f68
chenkui <jihe12180@gmail.com>
2021-10-05 20:42:43 +0800

    springcloud maven parent project

--- examples/springcloud/README.md
+++ examples/springcloud/README.md
@@ -1,3 +1,6 @@
+## 说明
+集成各种主流 SpringCloud 相关组件，完成组件 demo，进行源码分析，具体分析流程记录在[语雀文档](https://www.yuque.com/mg12180) 。
+
 #### springcloud
 springcloud 相关组件 demo
 
--- examples/springcloud/jihe-order/src/main/java/com/jihe/order/feign/FeignService.java
+++ examples/springcloud/jihe-order/src/main/java/com/jihe/order/feign/FeignService.java
@@ -2,9 +2,8 @@ package com.jihe.order.feign;
 
 import com.jihe.order.feign.fallback.FeignServiceFallBack;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @description: feign 服务
@@ -14,6 +13,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(value = "jihe-user",fallback = FeignServiceFallBack.class)
 public interface FeignService {
 
-    @RequestMapping(value = "/user/{id}",method = RequestMethod.GET)
+    @GetMapping(value = "/user/{id}")
     String getUser(@PathVariable("id") int id);
 }
