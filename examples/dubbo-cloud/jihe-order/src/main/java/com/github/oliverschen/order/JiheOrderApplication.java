82dcc74bf3f15089c8d1c73adf727c5ddc5cc3e0
chenkui <jihe12180@gmail.com>
2021-10-07 22:38:45 +0800

    add order service

---
+++ examples/dubbo-cloud/jihe-order/src/main/java/com/github/oliverschen/order/JiheOrderApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiheOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiheOrderApplication.class, args);
+    }
+
+}
---
+++ examples/dubbo-cloud/jihe-order/src/test/java/com/github/oliverschen/order/JiheOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiheOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
