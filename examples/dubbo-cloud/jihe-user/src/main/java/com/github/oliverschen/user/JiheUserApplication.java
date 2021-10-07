549ad46ed80d4b3c5ff376e1ec4f424ff7089715
chenkui <jihe12180@gmail.com>
2021-10-07 22:40:26 +0800

    add user service

---
+++ examples/dubbo-cloud/jihe-user/src/main/java/com/github/oliverschen/user/JiheUserApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiheUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JiheUserApplication.class, args);
+    }
+
+}
---
+++ examples/dubbo-cloud/jihe-user/src/test/java/com/github/oliverschen/user/JiheUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiheUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
