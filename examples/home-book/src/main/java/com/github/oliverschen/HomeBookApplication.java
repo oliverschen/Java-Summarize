e2b667d60faa792d4b2999b2aac837608b3b5804
chenkui <jihe12180@gmail.com>
2021-12-29 00:21:48 +0800

    create project

---
+++ examples/home-book/src/main/java/com/github/oliverschen/HomeBookApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeBookApplication.class, args);
+	}
+
+}
---
+++ examples/home-book/src/test/java/com/github/oliverschen/HomeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
