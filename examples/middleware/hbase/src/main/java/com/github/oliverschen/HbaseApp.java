941b63f710d309ce6e5aa7faa4f1cb88ada18964
chenkui-jk <chenkui-jk@360shuke.com>
2022-07-13 21:34:01 +0800

    first commit

---
+++ examples/middleware/hbase/src/main/java/com/github/oliverschen/HbaseApp.java
@@ -0,0 +1,15 @@
+package com.github.oliverschen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class HbaseApp {
+    public static void main( String[] args ) {
+        SpringApplication.run(HbaseApp.class,args);
+    }
+}
---
+++ examples/middleware/kafka/src/test/java/com/github/oliverschen/KafkaAppTest.java
@@ -0,0 +1,20 @@
+package com.github.oliverschen;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class KafkaAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
